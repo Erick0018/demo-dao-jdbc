f9eb530f4d9a83e37e010b0d23316331a21f6c09
Erick Jesus <erickontiveros017@gmail.com>
2025-05-18 20:11:48 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,7 +14,9 @@ public class Program {
 
         Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.0, obj);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
-        
+
     }
 }
